271bf5439ca310c6b86f8eb4ec78d927b8e25bc0
= <=>
2021-03-13 16:04:58 +0000

    Package renamed

--- src/main/java/com/springprojects/petclinic/PetclinicApplication.java
+++ src/main/java/com/springprojects/petclinic/PetclinicApplication.java
@@ -1,4 +1,4 @@
-package com.petclinic;
+package com.springprojects.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/springprojects/petclinic/PetclinicApplicationTests.java
+++ src/test/java/com/springprojects/petclinic/PetclinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.petclinic;
+package com.springprojects.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
